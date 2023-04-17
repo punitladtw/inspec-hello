a6be53f037272473ef4705af5d2d7d4a28ceab31
Punit Lad <plad@thoughtworks.com>
2023-04-17 16:22:34 -0400

    validate writer and reader zones are different

--- controls/example.rb
+++ controls/example.rb
@@ -1,9 +1,22 @@
-control "multi-az configuration" do
+control "RDS clusters must be configured for multi AZ" do
   input('rds_clusters').each do |rds_cluster|
+    client = Aws::RDS::Client.new
+    cluster = client.describe_db_clusters({db_cluster_identifier: rds_cluster}).db_clusters[0]
+    describe cluster["db_cluster_identifier"] do
+      it "configured for multi AZ" do
+        cluster['multi_az'].should be true
+        cluster['db_cluster_members'].count.should be >= 2
+      end
 
-    describe aws_rds_cluster(db_cluster_identifier: rds_cluster) do
-      its('multi_az') { should be true }
-      its('db_cluster_members.count') { should be >= 2 }
+      writer = cluster['db_cluster_members'].find { |x| x['is_cluster_writer'] }
+      readers = cluster['db_cluster_members'].select { |x| !x['is_cluster_writer'] }
+      readers.each do |reader| 
+        it "readers are configured outside of writer availability zone" do
+          writer_instance = aws_rds_instance(writer["db_instance_identifier"])
+          reader_instance = aws_rds_instance(reader["db_instance_identifier"])
+          expect(reader_instance.availability_zone).to_not eq(writer_instance.availability_zone)
+        end 
+      end
     end
   end
 end
