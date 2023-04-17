03c65886b300c780377c38e4f4b93ec31e4fe067
Punit Lad <plad@thoughtworks.com>
2023-04-17 12:41:29 -0400

    inspec on multiple clusters against availability zones

--- controls/example.rb
+++ controls/example.rb
@@ -1,5 +1,7 @@
-control "aws s3 buckets" do
-  describe aws_s3_buckets do
-    it { should exist }
+control "aws rds clusters" do
+  input('rds_clusters').each do |rds_cluster|
+    describe aws_rds_cluster(db_cluster_identifier: rds_cluster) do
+      its('availability_zones') { should eq ["us-east-1b", "us-east-1a", "us-east-1e"] }
+    end
   end
 end
