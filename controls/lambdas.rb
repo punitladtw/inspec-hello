a10d096c5cb5e3ed2a79390cf90f7ea479a4f739
Joe Fraser <joe.fraser@thoughtworks.com>
2023-04-18 17:31:23 -0300

    update lambdas test for az compliance

--- controls/lambdas.rb
+++ controls/lambdas.rb
@@ -4,6 +4,17 @@ control "Lambdas must be configured for multi AZ" do
         unless lambda_function.vpc_config.nil?  
             describe lambda_function do          
                 its ('vpc_config.subnet_ids.count') { should be >= 2}
+                
+                it "atleast one subnet should be in a different availability zone" do
+                    subnet_ids = lambda_function.vpc_config.subnet_ids
+                    first_subnet = aws_subnet(subnet_ids.first)
+                    all_other_subnets = subnet_ids.last(subnet_ids.length - 1)
+                    subnets_in_other_azs = all_other_subnets.select do |x| 
+                        aws_subnet(x).availability_zone != first_subnet.availability_zone 
+                    end
+                    
+                    subnets_in_other_azs.count.should be > 0
+                end
             end
         end
     end
